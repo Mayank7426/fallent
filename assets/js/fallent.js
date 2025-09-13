2dc022a2006b1d828d42511015ab4c039ef6a530
Mayank7568 <mayankmathur7426@gmail.com>
2025-09-13 13:36:03 -0500

    first commit

---
+++ assets/js/fallent.js
@@ -0,0 +1,8 @@
+function rr() {
+     var menu=document.querySelector('.navmenu');
+      menu.classList.toggle('show');
+
+      var demo=document.querySelector('.mm-one i');
+           demo.classList.toggle('fa-close');
+           demo.classList.toggle('fa-bars');
+}
